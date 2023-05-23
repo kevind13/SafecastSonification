9b0d34f4fc6264c1e65d70f0401d38f155093980
Kevin <kevin@preki.com>
2023-05-23 14:58:49 +0900

    feat: add loop to reproduce sonification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,130 +1,271 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import style from './App.module.css';
 import Footer from './components/Footer/Footer';
 import Button from './components/Button/Button';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { PulseLoader } from 'react-spinners';
-import { fetchDevices } from './services/devices';
-import { MidiPlayer } from 'midi-sounds-react';
-
-
-type Sensor = {
-  id: number;
-  sensor: string | undefined;
-  component: number | undefined;
-};
+import { startSonification } from './services/sonification';
+import { Midi } from '@tonejs/midi';
+import * as Tone from 'tone';
 
 export type Device = {
   id: number;
-  manufacturer: any;
-  model: any;
-  sensor: any;
-  number_measurements: number | undefined;
+  device: string | undefined;
+  component: number | undefined;
 };
 
-function App() {
-  const [sensors, setSensors] = useState<Sensor[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [devices, setDevices] = useState<Device[]>([])
+export type Notes = {
+  note: number;
+  time: number;
+  type: 'note_on' | 'note_off';
+  velocity: number;
+};
 
-  const addSensor = () => {
-    const newSensor: Sensor = {
-      id: sensors.length + 1,
-      sensor: undefined,
+let synths:  Tone.PolySynth<Tone.Synth<Tone.SynthOptions>>[] = []
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [loadingText, setLoadingText] = useState<string | undefined>();
+  const [devices, setDevices] = useState<Device[]>([]);
+  const [error, setError] = useState<string | undefined>();
+  const [notes, setNotes] = useState<Notes[] | undefined>();
+  const [sonificationInterval, setSonificationInterval] = useState<NodeJS.Timeout | null>(null);
+  // const [synths, setSynths] = useState<Tone.PolySynth<Tone.Synth<Tone.SynthOptions>>[]>([]);
+
+  const addDevice = () => {
+    setError('');
+    const newDevice: Device = {
+      id: devices.length + 1,
+      device: undefined,
       component: undefined,
     };
-
-    setSensors([...sensors, newSensor]);
+    setDevices([...devices, newDevice]);
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetchDevices();
-        // const response = await fetch('https://api.safecast.org/devices/'); 
-        console.log(response)
-        // if (response) {
-        //   const data = await response.json();
-        //   const options = data.map((item: any) => ({
-        //     value: item.id,
-        //     label: item.name,
-        //   }));
-        //   console.log(options);
-        //   setDevices(options);
-        // }
-      } catch (error) {
-        console.error('Error fetching options:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   // Function to remove a sensor
-  const removeSensor = (id: number) => {
-    setSensors((prevSensors) => prevSensors.filter((sensor) => sensor.id !== id));
+  const removeDevice = (id: number) => {
+    setError('');
+    setDevices((prevDevices) => prevDevices.filter((device) => device.id !== id));
   };
 
   // Function to handle name change
-  const handleNameChange = (id: number, newName: string) => {
-    setSensors((prevSensors) =>
-      prevSensors.map((sensor) => (sensor.id === id ? { ...sensor, name: newName } : sensor)),
+  const handleDeviceChange = (id: number, newName: string) => {
+    setDevices((prevDevices) =>
+      prevDevices.map((device) => (device.id === id ? { ...device, device: newName } : device)),
     );
   };
 
   // Function to handle value change
-  const handleValueChange = (id: number, newValue: number) => {
-    setSensors((prevSensors) =>
-      prevSensors.map((sensor) => (sensor.id === id ? { ...sensor, value: newValue } : sensor)),
+  const handleComponentChange = (id: number, newValue: number | undefined) => {
+    setDevices((prevDevices) =>
+      prevDevices.map((device) => (device.id === id ? { ...device, component: newValue } : device)),
     );
   };
 
-  // https://api.safecast.org/devices.json
-  const midiBase64 = "TVRoZAAAAAYAAQAEBABNVHJrAAABqwD/AwdTb3ByYW5vAMAAAOAAQADAAAD/WQIAAQD/WAQEAhgIAJBMWpAAgEwAAJBPWpAAgE8AAJBMWogAgEwAAJBMWogAgEwAAJBMWogAgEwAAJBMWogAgEwAAJBNWpAAgE0AAJBMWpAAgEwAAJBKWpAAgEoAAJBKWpAAgEoAAJBIWqAAgEgAAJBMWpAAgEwAAJBPWpAAgE8AAJBKWogAgEoAAJBKWogAgEoAAJBKWogAgEoAAJBFWogAgEUAAJBIWpAAgEgAAJBHWpAAgEcAAJBFWqAAgEUAAJBMWpAAgEwAAJBPWpAAgE8AAJBMWogAgEwAAJBMWogAgEwAAJBMWogAgEwAAJBMWogAgEwAAJBNWpAAgE0AAJBMWpAAgEwAAJBKWpAAgEoAAJBKWpAAgEoAAJBIWqAAgEgAAJBMWpAAgEwAAJBPWpAAgE8AAJBKWogAgEoAAJBKWogAgEoAAJBKWogAgEoAAJBKWogAgEoAAJBKWpAAgEoAAJBFWogAgEUAAJBHWogAgEcAAJBIWpAAgEgAAJBHWpAAgEcAAJBFWoGAAIBFAAD/LwBNVHJrAAAB1QD/AwRBbHRvAMAAAOAAQADAAAD/WQIAAQD/WAQEAhgIAJBFWpAAgEUAAJBDWpAAgEMAAJBDWogAgEMAAJBEWogAgEQAAJBFWogAgEUAAJBHWogAgEcAAJBFWogAgEUAAJBDWpAAgEMAAJBFWogAgEUAAJBFWpAAgEUAAJBDWpAAgEMAAJBDWqAAgEMAAJBDWqAAgEMAAJBDWogAgEMAAJBDWogAgEMAAJBBWogAgEEAAJBFWogAgEUAAJBFWpAAgEUAAJBEWpAAgEQAAJBAWqAAgEAAAJBFWpAAgEUAAJBHWpAAgEcAAJBDWpgAgEMAAJBEWogAgEQAAJBFWogAgEUAAJBHWogAgEcAAJBIWpgAgEgAAJBIWogAgEgAAJBIWogAgEgAAJBHWogAgEcAAJBDWqAAgEMAoACQQ1qIAIBDAACQRVqIAIBFAACQR1qIAIBHAACQSFqIAIBIAACQSlqQAIBKAACQPlqQAIA+AACQQFqQAIBAAACQQFqIAIBAAACQPlqIAIA+AACQPVqIAIA9AACQQFqIAIBAAACQRVqIAIBFAACQQ1qIAIBDAACQQVqwAIBBAACQQFqIAIBAAACQPlqIAIA+AACQQFqgAIBAAAD/LwBNVHJrAAACDAD/AwVUZW5vcgDAAADgAEAAwAAA/1kCAAEA/1gEBAIYCACQPFqQAIA8AACQPlqQAIA+AACQQFqIAIBAAACQPlqIAIA+AACQPFqIAIA8AACQO1qIAIA7AACQPFqIAIA8AACQPlqQAIA+AACQPFqIAIA8AACQPFqQAIA8AACQO1qQAIA7AACQQFqgAIBAAACQPFqgAIA8AACQO1qIAIA7AACQPlqIAIA+AACQOVqIAIA5AACQPlqIAIA+AACQQFqYAIBAAACQPlqIAIA+AACQPFqgAIA8AACQPFqQAIA8AACQPlqQAIA+AACQQFqIAIBAAACQPlqIAIA+AACQPFqIAIA8AACQO1qIAIA7AACQPFqIAIA8AACQPlqIAIA+AACQN1qIAIA3AACQQ1qIAIBDAACQRVqQAIBFAACQQ1qIAIBDAACQPlqIAIA+AACQQFqgAIBAAJAAkDdaiACANwAAkDlaiACAOQAAkDtaiACAOwAAkDxaiACAPAAAkD5amACAPgAAkEBaiACAQAAAkEJaiACAQgAAkERaiACARAAAkEVakACARQAAkENaiACAQwAAkEFaiACAQQAAkEBaiACAQAAAkD1aiACAPQAAkD5aiACAPgAAkEBakACAQAAAkDlaiACAOQAAkD5aoACAPgAAkD1aiACAPQAAkDtaiACAOwAAkD1aoACAPQAA/y8ATVRyawAAAi4A/wMEQmFzcwDAAADgAEAAwAAA/1kCAAEA/1gEBAIYCACQOVqQAIA5AACQO1qQAIA7AACQPFqIAIA8AACQO1qIAIA7AACQOVqIAIA5AACQOFqIAIA4AACQOVqIAIA5AACQO1qIAIA7AACQPFqIAIA8AACQOVqIAIA5AACQNVqIAIA1AACQMlqIAIAyAACQN1qQAIA3AACQMFqgAIAwAACQPFqQAIA8AACQNFqQAIA0AACQN1qIAIA3AACQO1qIAIA7AACQPlqIAIA+AACQNVqIAIA1AACQNFqIAIA0AACQMlqIAIAyAACQNFqQAIA0AACQLVqgAIAtAACQOVqQAIA5AACQN1qQAIA3AACQPFqIAIA8AACQPlqIAIA+AACQQFqYAIBAAACQPlqIAIA+AACQPFqIAIA8AACQNFqIAIA0AACQNVqIAIA1AACQMlqIAIAyAACQN1qQAIA3AACQMFqgAIAwAACQMFqIAIAwAACQMlqIAIAyAACQNFqIAIA0AACQNVqIAIA1AACQN1qYAIA3AACQOVqIAIA5AACQO1qIAIA7AACQPFqIAIA8AACQPlqQAIA+AACQMFqIAIAwAACQMlqIAIAyAACQNFqIAIA0AACQNVqIAIA1AACQN1qQAIA3AACQNVqIAIA1AACQNFqIAIA0AACQMlqIAIAyAACQNFqIAIA0AACQNVqIAIA1AACQN1qIAIA3AACQOVqgAIA5AACQLVqgAIAtAAD/LwA="
-  
+  const validateInputs = () => {
+    const filteredDevices = devices.filter(
+      (device) => device.component === undefined || !device.device || device.device.trim() === '',
+    );
+
+    if (filteredDevices.length) {
+      setError('Fill all the inputs');
+      return false;
+    }
+
+    const uniqueComponents = devices.map((device) => device.component);
+    if (devices.length !== new Set(uniqueComponents).size) {
+      setError('Duplicate component values are not allowed.');
+      return false;
+    }
+
+    // Check for valid component values (between 1 and 7)
+    const invalidComponents = uniqueComponents.filter((value) => value === undefined || value < 1 || value > 7);
+    if (invalidComponents.length > 0) {
+      setError('Component values should be between 1 and 7.');
+      return false;
+    }
+
+    // All validations passed
+    return true;
+  };
+
+  const convertDataToArrayBuffer = (data: string) => {
+    const binaryString = atob(data);
+    const bytes = new Uint8Array(binaryString.length);
+
+    for (let i = 0; i < binaryString.length; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+  };
+
+  const handleStartSonification = async () => {
+    try {
+      setError('');
+
+      const isValid = validateInputs();
+      if (!isValid) {
+        setLoading(false);
+        return;
+      }
+      setLoadingText('Generating the song');
+      setLoading(true);
+
+      const notes = await startSonification(devices);
+      setLoadingText('Playing the song');
+      if (!notes?.data) return;
+
+      const arrayBuffer = convertDataToArrayBuffer(notes.data);
+      const midi = new Midi(arrayBuffer);
+      const now = Tone.now() + 0.5;
+      midi.tracks.forEach((track) => {
+        const synth = new Tone.PolySynth(Tone.Synth, {
+          envelope: {
+            attack: 0.02,
+            decay: 0.1,
+            sustain: 0.3,
+            release: 1,
+          },
+        }).toDestination();
+
+        synths.push(synth);
+
+        //schedule all of the events
+        track.notes.forEach((note) => {
+          synth.triggerAttackRelease(note.name, note.duration, note.time + now, note.velocity);
+        });
+      });
+
+      setNotes(notes?.data);
+
+      setSonificationInterval(
+        setInterval(async () => {
+          setLoadingText('Generating the song');
+          const notes = await startSonification(devices);
+          if (!notes?.data) return;
+
+          const arrayBuffer = convertDataToArrayBuffer(notes.data);
+          const midi = new Midi(arrayBuffer);
+          const now = Tone.now();
+          
+          const temp_synths = [];
+
+          while (synths.length) {
+            temp_synths.push(synths.shift());
+          }
+
+          midi.tracks.forEach((track) => {
+            const synth = new Tone.PolySynth(Tone.Synth, {
+              envelope: {
+                attack: 0.02,
+                decay: 0.1,
+                sustain: 0.3,
+                release: 1,
+              },
+            }).toDestination();
+            
+            
+            synths.push(synth);
+
+            //schedule all of the events
+            track.notes.forEach((note) => {
+              synth.triggerAttackRelease(note.name, note.duration, note.time + now, note.velocity);
+            });
+          });         
+
+          while (temp_synths.length) {
+            const synth = temp_synths.shift();
+            if (synth) synth.disconnect();
+          }
+
+          setLoadingText('Playing the song');
+          setNotes(notes?.data);
+        }, 12000),
+      );
+    } catch (e) {
+      setError('An error occurred. Please try again.');
+      console.error(error);
+    }
+  };
+
+  const handleStopSonification = useCallback(() => {
+    setLoading(false);
+    if (sonificationInterval) {
+      clearInterval(sonificationInterval);
+      setSonificationInterval(null);
+    }
+
+    while (synths.length) {
+      const synth = synths.shift();
+      if (synth) synth.disconnect();
+    }
+
+  }, [sonificationInterval]);
+
   return (
     <div className={style.container}>
       <div className={style.title}>
         <h1>SONIFICATION OF SAFECAST SENSORS</h1>
         <div />
       </div>
-      <audio autoPlay controls>
-        <source
-          src={midiBase64}
-        />
-        Your browser does not support the audio element.
-      </audio>
       {loading && (
-        <div className={style.loader}>
-          <h2>Loading...</h2>
-          <PulseLoader color="white" loading={loading} size={30} aria-label="Loading Spinner" data-testid="loader" />
+        <div className={style.playingSong}>
+          <div className={style.loader}>
+            <h2>{loadingText}</h2>
+            <PulseLoader color="white" loading={loading} size={30} aria-label="Loading Spinner" data-testid="loader" />
+          </div>
+          <Button title="Stop Sonification" className={style.add} onClick={handleStopSonification} />
+          <div className={style.notesContainer}>
+            <h2>Notes:</h2>
+            <textarea value={JSON.stringify(notes, null, 2)} readOnly className={style.notesTextArea} />
+          </div>
         </div>
       )}
       {!loading && (
         <div className={style.content}>
+          <div className={style.buttons}>
+            {devices.length !== 7 && (
+              <Button
+                title={devices.length === 0 ? 'Add a mapper' : 'Add more mappers'}
+                className={style.add}
+                onClick={addDevice}
+              />
+            )}
+            {devices.length >= 1 && (
+              <Button title="Start sonification" className={style.add} onClick={handleStartSonification} />
+            )}
+          </div>
           <div className={style.list}>
-            {sensors.map((sensor) => (
-              <div key={sensor.id} className={style.sensorRow}>
+            {devices.length >= 1 && (
+              <div className={style.inputTitle}>
+                <h2>Device</h2>
+                <h2>Component</h2>
+              </div>
+            )}
+            {devices.map((device) => (
+              <div key={device.id} className={style.deviceRow}>
                 <input
                   type="text"
-                  value={sensor.sensor}
-                  onChange={(e) => handleNameChange(sensor.id, e.target.value)}
-                  className={style.sensorInput}
+                  value={device.device}
+                  onChange={(e) => handleDeviceChange(device.id, e.target.value)}
+                  className={style.deviceInput}
                 />
                 <input
                   type="text"
-                  value={sensor.component}
-                  onChange={(e) => handleValueChange(sensor.id, parseInt(e.target.value))}
-                  className={style.sensorInput}
+                  value={device.component || ''}
+                  onChange={(e) => {
+                    const newValue = e.target.value.trim();
+                    handleComponentChange(device.id, newValue === '' ? undefined : parseInt(newValue));
+                  }}
+                  className={style.deviceInput}
                 />
-                <button className={style.deleteButton} onClick={() => removeSensor(sensor.id)}>
+                <button className={style.deleteButton} onClick={() => removeDevice(device.id)}>
                   <RiDeleteBin6Line />
                 </button>
               </div>
             ))}
           </div>
-          {sensors.length !== 7 && <Button title="Add more mappers" className={style.add} onClick={addSensor} />}
-          {sensors.length >= 1 && <Button title="Start sonification" className={style.add} />}
+          {error && <div className={style.error}>{error}</div>}
         </div>
       )}
       <Footer />
