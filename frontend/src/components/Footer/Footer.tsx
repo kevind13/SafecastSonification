2ff34d2ba4a0b098d4f7549ca9069098071a8487
Kevin <kevin@preki.com>
2023-06-13 14:39:11 +0900

    feat: add lab infor to footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,12 +3,20 @@ import style from './Footer.module.css';
 const Footer = () => {
   return (
     <div className={style.container}>
-      <span>De Colombia pal mundo</span>
-      <img
-        src="https://assets.website-files.com/60d7d2e0002c46899b0aeeb8/61003abf691b46bc8eed1fb9_Colombia_Heart.png"
-        loading="lazy"
-        alt="heart"
-      />
+      {/* <div>
+        <span>De Colombia pal mundo</span>
+        <img
+          src="https://assets.website-files.com/60d7d2e0002c46899b0aeeb8/61003abf691b46bc8eed1fb9_Colombia_Heart.png"
+          loading="lazy"
+          alt="heart"
+        />
+      </div> */}
+      <a href='https://onkyo.u-aizu.ac.jp/#/'>
+      Onkyo laboratory
+      </a>
+      <div>
+      University of Aizu
+      </div>
     </div>
   );
 };
